5bffbb677b06ecec4ec2b7bcbd16c889e6904618
aimanprasla <115961188+aimanprasla@users.noreply.github.com>
2024-07-14 19:28:30 -0500

    Update ChatsPage.jsx

--- frontend/src/ChatsPage.jsx
+++ frontend/src/ChatsPage.jsx
@@ -1,7 +1,7 @@
 import { MultiChatSocket, MultiChatWindow, useMultiChatLogic } from 'react-chat-engine-advanced'
 const ChatsPage = (props) => {
     const chatProps = useMultiChatLogic(
-        '468f8b0a-e791-4a97-b68e-1f957a3d7c95', 
+        '', //Enter your project ID in these quotes
         props.user.username, 
         props.user.secret
     );
